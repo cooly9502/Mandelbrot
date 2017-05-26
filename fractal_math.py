9da1b9bd8dde9a51935de90c7dd4dd419d0203b5
cooly9502 <techtag@hotmail.com>
2017-05-26 07:33:57 -0400

    compressed math

--- fractal_math.py
+++ fractal_math.py
@@ -1,28 +1,12 @@
-#makes math look neater
-sq = lambda x : x * x 
-#testing coords if in set function
-def testCoord(max_iter, x_in, y_in):
-    #Stores original coordinates
-    a, b, iteration = float(x_in), float(y_in), 1
-    #Iteration 1 is easier just to do this math...
-    c = sq(x_in) + sq(y_in)
-    if c >= 4:
-        return iteration
-    else:
-        max_iter -= 1
-        while max_iter != 0:
-            iteration += 1
-            z = {'x' : sq(a) - sq(b), 'y' : 2*a*b}
-            a, b = z['x'] + x_in, z['y'] + y_in
-            if sq(a) + sq(b) >= 4:
-                return iteration
-                max_iter = 0
-            elif max_iter == 1:
-                return iteration
-            else:
-                max_iter -= 1
-                pass #(move on to next iteration)
-            
-
-
+#makes my math look neater 
+sq = lambda x : x*x
+c = lambda x, y: sq(x) + sq(y) #x^2 + y^2
+def testCoord(max_iter, x, y):
+     #a,b = original coords
+     a, b, iteration = float(x), float(y), 1
+     while c(x,y) < 4 and iteration < max_iter:
+         chekpntx=x ; x = ( sq(x) - sq(y) ) + a
+         y = (2*chekpntx*y) + b
+         iteration += 1
+     return iteration 
         
